50a584812e881fb2ba5251fde39d8d8bcc42dad3
Rakesh Debbarma <yarwngmathematics@gmail.com>
2026-05-10 19:28:13 +0530

    fixed client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 export default function Home() {
   const scriptURL = "https://script.google.com/macros/s/AKfycbyXxSYFEtcKMLA3AU4h1pKJIgl76zA-L_Z8HoCEqyJubQKOT7TCEIfILryMaAR7Kf0e/exec"
   const handleSubmit = async (e: any) => {
