f40a8f3d73c933a94d33fcb534e956144f0fd95f
yarwngmathematics <yarwngmathematics@gmail.com>
2026-05-17 17:32:50 +0530

    Update firebase.ts

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -2,15 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSuWWt-sD6hQxQjKYj-G0SqdUjlbASPm0",
-  authDomain: "yarwng-mathematics01.firebaseapp.com",
-  projectId: "yarwng-mathematics01",
-  storageBucket: "yarwng-mathematics01.firebasestorage.app",
-  messagingSenderId: "699931202912",
-  appId: "1:699931202912:web:e8a6a09627775d86a6ba05",
-  measurementId: "G-5GCZRVFH68"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export default app;
