c0eaeed866e584ec72125564ea8a796ed1c09df5
Rakesh Debbarma <yarwngmathematics@gmail.com>
2026-05-24 21:14:22 +0530

    Added robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://yarwngmathematicsonline.vercel.app/sitemap.xml",
+  };
+}
