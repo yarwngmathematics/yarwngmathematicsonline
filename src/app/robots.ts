5816aa9000709c6c9a364ceb25ba21dd76056bf5
Rakesh Debbarma <yarwngmathematics@gmail.com>
2026-05-25 14:48:23 +0530

    version 2.4

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://yarwngmathematicsonline.vercel.app/sitemap.xml',
+    sitemap: 'https://yarwngmathematics.com/sitemap.xml',
   }
 }
